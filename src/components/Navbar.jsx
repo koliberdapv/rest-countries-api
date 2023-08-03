0177e6ee6cdbca709773ff35453f2262755d5fd1
Paul Koliberda <canrando4567@gmail.com>
2023-08-03 21:39:39 +0300

    dark mode finished + local storage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,38 @@
 import styled from 'styled-components';
 import { BsMoon } from 'react-icons/bs';
 import { BsMoonFill } from 'react-icons/bs';
+import { useEffect, useState, useRef } from 'react';
+
+const isDarkModeEnabled = localStorage.getItem('dark-mode') === 'enabled';
 
 const Navbar = () => {
+	const [darkMode, setDarkMode] = useState(isDarkModeEnabled);
+	const handleClick = () => {
+		if (!darkMode) {
+			setDarkMode(true);
+			localStorage.setItem('dark-mode', 'enabled');
+		}
+		if (darkMode) {
+			setDarkMode(false);
+			localStorage.setItem('dark-mode', 'disabled');
+		}
+	};
+	useEffect(() => {
+		if (!darkMode) document.body.classList.remove('dark-mode');
+		if (darkMode) document.body.classList.add('dark-mode');
+	}, [darkMode]);
+
 	return (
 		<Wrapper>
 			<div className="container">
 				<div className="navbar">
 					<h4>Where in the world?</h4>
-					<button className="dark-mode">
-						<BsMoon />
-						{/* <BsMoonFill /> */}
+					<button
+						className="dark-mode-btn"
+						aria-label="dark-mode"
+						onClick={handleClick}
+					>
+						{darkMode ? <BsMoonFill color="white" /> : <BsMoon />}
 						<span>Dark Mode</span>
 					</button>
 				</div>
@@ -34,9 +56,11 @@ const Wrapper = styled.nav`
 		text-transform: none;
 		font-weight: var(--fw-800);
 	}
-	.dark-mode {
+	.dark-mode-btn {
 		display: flex;
 		gap: 0.5rem;
+		height: 3rem;
+		align-items: center;
 	}
 	@media screen and (width < 500px) {
 		h4 {
